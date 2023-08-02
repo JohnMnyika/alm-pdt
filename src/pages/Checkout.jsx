c839ac1db019ee3cffd1ea0c1b2a4628862cba91
JohnMnyika <jmnyika514@gmail.com>
2023-08-02 16:32:12 +0300

    Introduced mpesa payment method, Switched from fakeapi to local storage

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Footer, Navbar } from "../components";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+
 const Checkout = () => {
   const state = useSelector((state) => state.handleCart);
 
@@ -24,42 +25,25 @@ const Checkout = () => {
     let subtotal = 0;
     let shipping = 30.0;
     let totalItems = 0;
-    state.map((item) => {
-      return (subtotal += item.price * item.qty);
+    state.forEach((item) => {
+      subtotal += item.price * item.qty;
+      totalItems += item.qty;
     });
 
-    state.map((item) => {
-      return (totalItems += item.qty);
-    });
+    const handlePaymentSubmit = (event) => {
+      event.preventDefault();
+      const mpesaPhoneNumber = event.target.mpesaPhoneNumber.value;
+      // Here you can implement the logic for the M-Pesa payment processing
+      // For this example, we'll just show an alert with the M-Pesa phone number
+      alert(`M-Pesa Payment Successful. Phone Number: ${mpesaPhoneNumber}`);
+    };
+
     return (
       <>
         <div className="container py-5">
           <div className="row my-4">
             <div className="col-md-5 col-lg-4 order-md-last">
-              <div className="card mb-4">
-                <div className="card-header py-3 bg-light">
-                  <h5 className="mb-0">Order Summary</h5>
-                </div>
-                <div className="card-body">
-                  <ul className="list-group list-group-flush">
-                    <li className="list-group-item d-flex justify-content-between align-items-center border-0 px-0 pb-0">
-                      Products ({totalItems})<span>${Math.round(subtotal)}</span>
-                    </li>
-                    <li className="list-group-item d-flex justify-content-between align-items-center px-0">
-                      Shipping
-                      <span>${shipping}</span>
-                    </li>
-                    <li className="list-group-item d-flex justify-content-between align-items-center border-0 px-0 mb-3">
-                      <div>
-                        <strong>Total amount</strong>
-                      </div>
-                      <span>
-                        <strong>${Math.round(subtotal + shipping)}</strong>
-                      </span>
-                    </li>
-                  </ul>
-                </div>
-              </div>
+              {/* ... (Previous code) */}
             </div>
             <div className="col-md-7 col-lg-8">
               <div className="card mb-4">
@@ -67,131 +51,9 @@ const Checkout = () => {
                   <h4 className="mb-0">Billing address</h4>
                 </div>
                 <div className="card-body">
-                  <form className="needs-validation" novalidate>
+                  <form className="needs-validation" onSubmit={handlePaymentSubmit} noValidate>
                     <div className="row g-3">
-                      <div className="col-sm-6 my-1">
-                        <label for="firstName" className="form-label">
-                          First name
-                        </label>
-                        <input
-                          type="text"
-                          className="form-control"
-                          id="firstName"
-                          placeholder=""
-                          value=""
-                          required
-                        />
-                        <div className="invalid-feedback">
-                          Valid first name is required.
-                        </div>
-                      </div>
-
-                      <div className="col-sm-6 my-1">
-                        <label for="lastName" className="form-label">
-                          Last name
-                        </label>
-                        <input
-                          type="text"
-                          className="form-control"
-                          id="lastName"
-                          placeholder=""
-                          value=""
-                          required
-                        />
-                        <div className="invalid-feedback">
-                          Valid last name is required.
-                        </div>
-                      </div>
-
-                      <div className="col-12 my-1">
-                        <label for="email" className="form-label">
-                          Email
-                        </label>
-                        <input
-                          type="email"
-                          className="form-control"
-                          id="email"
-                          placeholder="you@example.com"
-                          required
-                        />
-                        <div className="invalid-feedback">
-                          Please enter a valid email address for shipping
-                          updates.
-                        </div>
-                      </div>
-
-                      <div className="col-12 my-1">
-                        <label for="address" className="form-label">
-                          Address
-                        </label>
-                        <input
-                          type="text"
-                          className="form-control"
-                          id="address"
-                          placeholder="1234 Main St"
-                          required
-                        />
-                        <div className="invalid-feedback">
-                          Please enter your shipping address.
-                        </div>
-                      </div>
-
-                      <div className="col-12">
-                        <label for="address2" className="form-label">
-                          Address 2{" "}
-                          <span className="text-muted">(Optional)</span>
-                        </label>
-                        <input
-                          type="text"
-                          className="form-control"
-                          id="address2"
-                          placeholder="Apartment or suite"
-                        />
-                      </div>
-
-                      <div className="col-md-5 my-1">
-                        <label for="country" className="form-label">
-                          Country
-                        </label>
-                        <br />
-                        <select className="form-select" id="country" required>
-                          <option value="">Choose...</option>
-                          <option>Kenya</option>
-                        </select>
-                        <div className="invalid-feedback">
-                          Please select a valid country.
-                        </div>
-                      </div>
-
-                      <div className="col-md-4 my-1">
-                        <label for="state" className="form-label">
-                          State
-                        </label>
-                        <br />
-                        <select className="form-select" id="state" required>
-                          <option value="">Choose...</option>
-                          <option>Nairobi</option>
-                        </select>
-                        <div className="invalid-feedback">
-                          Please provide a valid city.
-                        </div>
-                      </div>
-
-                      <div className="col-md-3 my-1">
-                        <label for="zip" className="form-label">
-                          Zip
-                        </label>
-                        <input
-                          type="text"
-                          className="form-control"
-                          id="zip"
-                          placeholder=""
-                          required
-                        />
-                        <div className="invalid-feedback">
-                          Zip code required.
-                        </div>
-                      </div>
+                      {/* ... (Previous code) */}
                     </div>
 
                     <hr className="my-4" />
@@ -199,80 +61,26 @@ const Checkout = () => {
                     <h4 className="mb-3">Payment</h4>
 
                     <div className="row gy-3">
+                      {/* M-Pesa phone number input */}
                       <div className="col-md-6">
-                        <label for="cc-name" className="form-label">
-                          Name on card
+                        <label htmlFor="mpesaPhoneNumber" className="form-label">
+                          M-Pesa Phone Number
                         </label>
                         <input
-                          type="text"
+                          type="tel"
                           className="form-control"
-                          id="cc-name"
-                          placeholder=""
+                          id="mpesaPhoneNumber"
+                          placeholder="Enter M-Pesa Phone Number"
                           required
                         />
-                        <small className="text-muted">
-                          Full name as displayed on card
-                        </small>
-                        <div className="invalid-feedback">
-                          Name on card is required
-                        </div>
-                      </div>
-
-                      <div className="col-md-6">
-                        <label for="cc-number" className="form-label">
-                          Credit card number
-                        </label>
-                        <input
-                          type="text"
-                          className="form-control"
-                          id="cc-number"
-                          placeholder=""
-                          required
-                        />
-                        <div className="invalid-feedback">
-                          Credit card number is required
-                        </div>
-                      </div>
-
-                      <div className="col-md-3">
-                        <label for="cc-expiration" className="form-label">
-                          Expiration
-                        </label>
-                        <input
-                          type="text"
-                          className="form-control"
-                          id="cc-expiration"
-                          placeholder=""
-                          required
-                        />
-                        <div className="invalid-feedback">
-                          Expiration date required
-                        </div>
-                      </div>
-
-                      <div className="col-md-3">
-                        <label for="cc-cvv" className="form-label">
-                          CVV
-                        </label>
-                        <input
-                          type="text"
-                          className="form-control"
-                          id="cc-cvv"
-                          placeholder=""
-                          required
-                        />
-                        <div className="invalid-feedback">
-                          Security code required
-                        </div>
+                        <small className="text-muted">Enter your M-Pesa phone number</small>
+                        <div className="invalid-feedback">M-Pesa phone number is required</div>
                       </div>
                     </div>
 
                     <hr className="my-4" />
 
-                    <button
-                      className="w-100 btn btn-primary "
-                      type="submit" disabled
-                    >
+                    <button className="w-100 btn btn-primary" type="submit" disabled>
                       Continue to checkout
                     </button>
                   </form>
@@ -284,6 +92,7 @@ const Checkout = () => {
       </>
     );
   };
+
   return (
     <>
       <Navbar />
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -7,6 +7,13 @@ import { addCart } from "../redux/action";
 
 import { Footer, Navbar } from "../components";
 
+// Import your product images
+import vinylMatImage from "../assets/vinylmatt.jpg";
+import cellucaBoardImage from "../assets/CelucaBoards.jpg";
+import frostedWindowVinylImage from "../assets/frostedwindows.jpg";
+import forexBoardImage from "../assets/forexboards.jpg";
+import transparentVinylImage from "../assets/transparentvinyl.jpg";
+
 const Product = () => {
   const { id } = useParams();
   const [product, setProduct] = useState([]);
@@ -21,18 +28,39 @@ const Product = () => {
   };
 
   useEffect(() => {
-    const getProduct = async () => {
+    const getProduct = () => {
       setLoading(true);
       setLoading2(true);
-      const response = await fetch(`https://fakestoreapi.com/products/${id}`);
-      const data = await response.json();
-      setProduct(data);
-      setLoading(false);
-      const response2 = await fetch(
-        `https://fakestoreapi.com/products/category/${data.category}`
+
+      const data = [
+        {
+          id: 1,
+          title: "Vinyl Mat",
+          description: "Description of Vinyl Mat",
+          price: 15.99,
+          image: vinylMatImage,
+          category: "Vinyl Mats",
+        },
+        {
+          id: 2,
+          title: "Celluca Board",
+          description: "Description of Celluca Board",
+          price: 25.99,
+          image: cellucaBoardImage,
+          category: "Celluca Boards",
+        },
+        // Add more products with their information and image paths
+      ];
+
+      const selectedProduct = data.find((item) => item.id === parseInt(id));
+      setProduct(selectedProduct);
+
+      const similarProductsData = data.filter(
+        (item) => item.category === selectedProduct.category
       );
-      const data2 = await response2.json();
-      setSimilarProducts(data2);
+      setSimilarProducts(similarProductsData);
+
+      setLoading(false);
       setLoading2(false);
     };
     getProduct();
@@ -128,46 +156,39 @@ const Product = () => {
       <>
         <div className="py-4 my-4">
           <div className="d-flex">
-            {similarProducts.map((item) => {
-              return (
-                <div key={item.id} className="card mx-4 text-center">
-                  <img
-                    className="card-img-top p-3"
-                    src={item.image}
-                    alt="Card"
-                    height={300}
-                    width={300}
-                  />
-                  <div className="card-body">
-                    <h5 className="card-title">
-                      {item.title.substring(0, 15)}...
-                    </h5>
-                  </div>
-                  {/* <ul className="list-group list-group-flush">
-                    <li className="list-group-item lead">${product.price}</li>
-                  </ul> */}
-                  <div className="card-body">
-                    <Link
-                      to={"/product/" + item.id}
-                      className="btn btn-dark m-1"
-                    >
-                      Buy Now
-                    </Link>
-                    <button
-                      className="btn btn-dark m-1"
-                      onClick={() => addProduct(item)}
-                    >
-                      Add to Cart
-                    </button>
-                  </div>
+            {similarProducts.map((item) => (
+              <div key={item.id} className="card mx-4 text-center">
+                <img
+                  className="card-img-top p-3"
+                  src={item.image}
+                  alt="Card"
+                  height={300}
+                  width={300}
+                />
+                <div className="card-body">
+                  <h5 className="card-title">
+                    {item.title.substring(0, 15)}...
+                  </h5>
                 </div>
-              );
-            })}
+                <div className="card-body">
+                  <Link to={"/product/" + item.id} className="btn btn-dark m-1">
+                    Buy Now
+                  </Link>
+                  <button
+                    className="btn btn-dark m-1"
+                    onClick={() => addProduct(item)}
+                  >
+                    Add to Cart
+                  </button>
+                </div>
+              </div>
+            ))}
           </div>
         </div>
       </>
     );
   };
+
   return (
     <>
       <Navbar />
@@ -175,12 +196,8 @@ const Product = () => {
         <div className="row">{loading ? <Loading /> : <ShowProduct />}</div>
         <div className="row my-5 py-5">
           <div className="d-none d-md-block">
-          <h2 className="">You may also Like</h2>
-            <Marquee
-              pauseOnHover={true}
-              pauseOnClick={true}
-              speed={50}
-            >
+            <h2 className="">You may also Like</h2>
+            <Marquee pauseOnHover={true} pauseOnClick={true} speed={50}>
               {loading2 ? <Loading2 /> : <ShowSimilarProduct />}
             </Marquee>
           </div>
